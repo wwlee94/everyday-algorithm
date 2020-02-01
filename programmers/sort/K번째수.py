23f050a9eab2ffde648f1dfa9b10c80733b9df92
wwlee94 <wwlee94@naver.com>
2020-02-02 02:29:38 +0900

    Fix: 프로그래머스 정렬 "K번째 수" 문제 풀이 수정

--- programmers/sort/K번째수.py
+++ programmers/sort/K번째수.py
@@ -1,6 +1,8 @@
 '''
 * 🙆‍♂️ Created by wwlee94 on 2020.02.01
-- 문제 풀이법 작성할 것
+1. command[0] -> LIST 자르는 시작 지점
+2. command[1] -> LIST 자르는 마지막 지점
+3. command[2] -> 1, 2에서 잘라서 나온 LIST의 몇 번째 원소인지
 '''
 def solution(array, commands):
     answer = []

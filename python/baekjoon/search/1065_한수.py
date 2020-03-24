0790c38c1271e720e36f6f51b5cf9fc16e9f1a49
wwlee94 <wwlee94@naver.com>
2020-03-24 23:54:23 +0900

    백준 탐색 "한수" Python 풀이

---
+++ python/baekjoon/search/1065_한수.py
@@ -0,0 +1,27 @@
+# https://www.acmicpc.net/problem/1065
+import sys
+input_s = lambda: sys.stdin.readline()
+
+result = 0
+N = input_s()
+
+def hansu(N):
+
+    if int(N) < 100:
+        result = int(N)
+        return result
+
+    else :
+        result = 99
+
+        for i in range(100,int(N)+1):
+            a = int(i / 100)
+            b = int(i % 100 / 10)
+            c = int(i % 10)
+            # 등차수열을 이루면!
+            if 2 * b == a + c:
+                result += 1
+
+    return result
+
+print(hansu(N))

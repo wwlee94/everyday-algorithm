27d432648e98fbe76fb65f6fc09d6c3950713502
wwlee94 <wwlee94@naver.com>
2020-04-21 00:26:35 +0900

    프로그래머스 연습문제 "핸드폰 번호 가리기" Python 추가

--- python/programmers/연습문제/LEVEL-1/핸드폰번호가리기.py
+++ python/programmers/연습문제/LEVEL-1/핸드폰번호가리기.py
@@ -2,6 +2,10 @@
 * 🙆‍♂️ Created by wwlee94 on 2020.04.19
 https://programmers.co.kr/learn/courses/30/lessons/12948
 # 프로그래머스 LV1 
+
+- 문제 풀이 접근 - 
+1. 뒤에서 4자리를 변경하라고 했으니 반복분을 뒤에서부터 돌면서
+2. 숫자인 요소 4개를 체크한 뒤 모든 문자는 '*' 로 변경 
 '''
 
 def solution(phone_number):
